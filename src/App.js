1b503eeee98507ad4c38896000434b032f32e114
exit <saraswathi.mac@gmail.com>
2022-02-09 22:23:09 +0530

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
-import Products from './components/Products';
+import { useState } from "react";
+import items from "./allData";
+import Menu from "./Components/Menu";
+import Button from "./Components/Button";
+
+const allCategories = ["All", ...new Set(items.map((item) => item.category))];
+
+console.log(allCategories);
 
 function App() {
+  const [menuItem, setMenuItem] = useState(items);
+  const [buttons, setButtons] = useState(allCategories);
+
+  //Filter Function
+  const filter = (button) => {
+    if (button === "All") {
+      setMenuItem(items);
+      return;
+    }
+
+    const filteredData = items.filter((item) => item.category === button);
+    setMenuItem(filteredData);
+  };
+
   return (
-    <>
-      <Products />
-    </>
+    <div className="App">
+      <div className="title">
+        <h1>
+          Yoga
+          <span> Mat</span>
+        </h1>
+      </div>
+      <Button button={buttons} filter={filter} />
+      <Menu menuItem={menuItem} />
+    </div>
   );
 }
 
---
+++ src/allData.js
@@ -0,0 +1,45 @@
+import side_bends from './images/side_bends.jpg';
+import vrikshasan from './images/vrikshasan.jpg';
+import meditation from './images/meditation.jpg';
+import running from './images/running.jpg';
+import skipping from './images/skipping.jpg';
+
+const data = [
+    {
+        id:1,
+        image: vrikshasan,
+        title: 'Tips and tricks to write good code',
+        category: 'Asana',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea, ab aperiam necessitatibus adipisci sunt facilis itaque non enim est!'
+    },
+    {
+        id:2,
+        image: meditation,
+        title: 'How to use Search Engine Optimisation',
+        category: 'Asana',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea, ab aperiam necessitatibus adipisci sunt facilis itaque non enim est!'
+    },
+    {
+        id:3,
+        image: skipping,
+        title: '10 Tricks and Tips in Python',
+        category: 'Exercise',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea, ab aperiam necessitatibus adipisci sunt facilis itaque non enim est!'
+    },
+    {
+        id:4,
+        image: running,
+        title: 'How to transition from Unreal Engine to Unity in simple steps',
+        category: 'Exercise',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea, ab aperiam necessitatibus adipisci sunt facilis itaque non enim est!'
+    },
+    {
+        id:5,
+        image: side_bends,
+        title: 'I think Fortnite is dead',
+        category: 'Pranayama',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea, ab aperiam necessitatibus adipisci sunt facilis itaque non enim est!'
+    }
+];
+
+export default data;
